d723b5804ba4f9c641f31610490497c70905d4cf
Aaron Scheu <amaridev@ceyu.de>
2016-11-05 17:39:59 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
     <img src="http://vuejs.org/images/logo.png">
     <h1>Our vigenere Code en & decoder</h1>
 
-    <vk-button-radio :value="radio" @change="radio = arguments[0]">
-    <vk-button value="encode">Encode</vk-button>
-    <vk-button value="decode">Decode</vk-button>
+    <vk-button-radio :value="radio" @change="radio = arguments[0]; vigenere()">
+      <vk-button :value="1" active>Encode</vk-button>
+      <vk-button :value="0">Decode</vk-button>
     </vk-button-radio>
 
     <div id="inputarea">
       <input v-model="key" placeholder="Enter your Key"></input><br>
-      <textarea v-model="message" @keyup="vigenere" placeholder="Enter your Message"></textarea>
+      <textarea v-model="message" v-on:keyup="vigenere" placeholder="Enter your Message"></textarea>
       <br>
       <h4>Output:</h4>
       <p style="white-space: pre; text-transform: uppercase">{{ output }}</p>
@@ -34,17 +34,23 @@ export default {
       var alphabet = 'abcdefghijklmnopqrstuvwxyz';
       var keyindex = 0;
       var output = '';
+
       for (var i = 0; i < this.message.length; i++) {
-        var num = alphabet.indexOf(this.message[i]);
+        var num = alphabet.indexOf(this.message.charAt(i));
         if (num !== -1) {
-          console.log(keyindex);
-          num += alphabet.indexOf(keyindex);
-          num %= alphabet.length+1;
+          if (this.radio == 1) {
+            num += alphabet.indexOf(this.key.charAt(keyindex));
+          } else {
+            num -= alphabet.indexOf(this.key.charAt(keyindex));
+          }
+          num %= alphabet.length;
           keyindex += 1;
           if (keyindex === this.key.length) {
             keyindex = 0;
           }
-          output += alphabet[num];
+          if (alphabet.charAt(num) != undefined) {
+            output += alphabet.charAt(num);
+          }
         }
       }
       this.output = output;
